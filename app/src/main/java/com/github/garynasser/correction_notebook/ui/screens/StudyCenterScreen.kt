f1a615f3d04af6be2d5e8e34bb40d3c1f4a5fab5
Fangwenky <2629920196@qq.com>
2026-03-13 15:20:01 +0800

    fix the login to HomeScreen route

--- app/src/main/java/com/github/garynasser/correction_notebook/ui/screens/StudyCenterScreen.kt
+++ app/src/main/java/com/github/garynasser/correction_notebook/ui/screens/StudyCenterScreen.kt
@@ -1,4 +1,4 @@
-package com.github.garynasser.correction_notebook.screens
+package com.github.garynasser.correction_notebook.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
