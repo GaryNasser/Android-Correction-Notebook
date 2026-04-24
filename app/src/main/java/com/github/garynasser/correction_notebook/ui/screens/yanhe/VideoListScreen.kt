9f49b906680265ad375c6956528a96dccf95e27c
Gary Nasser <garynasser73@gmail.com>
2026-04-24 09:44:56 +0800

    feat(yanhe): 新增获取屏幕录像功能

--- app/src/main/java/com/github/garynasser/correction_notebook/ui/screens/yanhe/VideoListScreen.kt
+++ app/src/main/java/com/github/garynasser/correction_notebook/ui/screens/yanhe/VideoListScreen.kt
@@ -1,6 +1,5 @@
 package com.github.garynasser.correction_notebook.ui.screens.yanhe
 
-import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,19 +9,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
@@ -42,7 +38,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.garynasser.correction_notebook.data.model.yanhe.CourseSection
@@ -107,10 +102,16 @@ fun CourseVideoListScreen(
                         items(state.videos) { video ->
                             VideoCard(
                                 section = video,
-                                onPlayClick = { videos ->
+                                onCameraPlayClick = { videos ->
                                     Log.i("VIDEO", "Play btn pressed")
                                     if (videos.isNotEmpty()) {
                                         onNavigateToPlayer(videos[0].mainUrl)
+                                    }
+                                },
+                                onScreenPlayClick = {videos ->
+                                    Log.i("VIDEO", "Play btn pressed")
+                                    if (videos.isNotEmpty()) {
+                                        onNavigateToPlayer(videos[0].vgaUrl)
                                     }
                                 }
                             )
@@ -125,7 +126,8 @@ fun CourseVideoListScreen(
 @Composable
 fun VideoCard(
     section: CourseSection,
-    onPlayClick: (List<Video>) -> Unit,
+    onCameraPlayClick: (List<Video>) -> Unit,
+    onScreenPlayClick: (List<Video>) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val timeInfo = buildString {
@@ -165,14 +167,26 @@ fun VideoCard(
             val hasVideo = section.videos.isNotEmpty()
 
             FilledIconButton(
-                onClick = { onPlayClick(section.videos) },
+                onClick = { onCameraPlayClick(section.videos) },
+                enabled = hasVideo,
+                modifier = Modifier.size(48.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Camera,
+                    contentDescription = "播放摄像头视频"
+                )
+            }
+
+            FilledIconButton(
+                onClick = { onScreenPlayClick(section.videos) },
                 enabled = hasVideo,
                 modifier = Modifier.size(48.dp),
                 shape = RoundedCornerShape(12.dp)
             ) {
                 Icon(
                     imageVector = Icons.Default.PlayArrow,
-                    contentDescription = "播放视频"
+                    contentDescription = "播放屏幕录像"
                 )
             }
         }
